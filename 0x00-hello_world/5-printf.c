ef903829648fb454b2afeacdf65dbe20f6ceaa0c
Phumnanya <111151661+Phumnanya@users.noreply.github.com>
2022-09-09 03:44:31 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-#include <string.h>
 /**
- * main - Entry point
- *
- * Return: always 0
- */
+* main - Entry point
+*
+* Return: always 0
+*/
 int main(void)
 { printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0); }
