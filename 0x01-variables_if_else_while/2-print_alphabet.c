a59cbcf6b920924cfca3c1e05f8ca84443501ecc
Phumnanya <eustace364@gmail.com>
2022-09-10 11:35:22 -0700

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,1 @@
-#include <stdio.h>
-/**
- * main - entry point
- *
- * Return: alway returns 0
- *
- */
-int main(void)
-{
-	char a;
-
-	for (a = 'a'; a <= 'z' ; a++)
-		putchar(a);
-	putchar('\n');
-	return (0);
-}
+2-print_alphabet.c
